6f9c32342573db77707cb796b61850fa57e8436b
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-10-28 15:34:46 +0300

    added array methods

--- index.js
+++ index.js
@@ -1,36 +1,85 @@
 // Write your solution here!
-let cats = ["Milo", "Otis", "Garfield"];
+//let cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name){
-    cats.push(name);
-}
+//function destructivelyAppendCat(name){
+//    cats.push(name);
+//}
+//
+//function destructivelyPrependCat(name){
+//    cats.unshift(name);
+//}
 
-function destructivelyPrependCat(name){
-    cats.unshift(name);
-}
+//function destructivelyRemoveLastCat(name){
+//    cats.pop(name);
+//}
 
-function destructivelyRemoveLastCat(name){
-    cats.pop(name);
-}
+//function destructivelyRemoveFirstCat(name){
+//    cats.shift(name);
+//}
 
-function destructivelyRemoveFirstCat(name){
-    cats.shift(name);
-}
+//function appendCat(name){
+//    return[...cats, name];
+//}
 
-function appendCat(name){
-    return[...cats, name];
-}
-
-function prependCat(name){
-    return[name, ...cats];
-}
+//function prependCat(name){
+//    return[name, ...cats];
+//}
 //console.log(cats);
 
 //const removeLastCat = cats.slice[-1];
-function removeLastCat(){
-    return cats.slice(0, -1);
-}
+//function removeLastCat(){
+//    return cats.slice(0, -1);
+//}
 //const removeFirstCat = cats.slice[1];
+//function removeFirstCat(){
+//    return cats.slice(1);
+//}
+
+
+// Write your solution here!
+let cats = [];
+cats.push(`Milo`, `Otis`, `Garfield`)
+console.log(cats);
+
+function destructivelyAppendCat(){
+cats.push('Ralph');
+return cats;
+}
+
+function destructivelyPrependCat(){
+cats.unshift("Bob")
+return cats;
+}
+
+function destructivelyRemoveLastCat(){
+cats.pop()
+return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+cats.shift()
+return cats;
+}
+
+function appendCat(){
+cats = [...cats, `Broom`]
+return cats;
+}
+
+function prependCat(){
+cats.pop();
+cats = [`Arnold`, ...cats]
+return cats;
+}
+
+function removeLastCat(){
+cats.shift();
+cats.splice(-1);
+return cats;
+}
+
 function removeFirstCat(){
-    return cats.slice(1);
+cats.push(`Garfield`);
+cats.shift()
+return cats;
 }
